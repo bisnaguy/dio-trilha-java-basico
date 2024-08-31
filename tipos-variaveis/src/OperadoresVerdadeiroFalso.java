82e502d820ffb6c9d75420e8530b75bbae683cb0
Gabriel Azambuja Silva <96156017+bisnaguy@users.noreply.github.com>
2024-08-31 09:39:03 -0300

    [feat]OperadorRelacional

---
+++ tipos-variaveis/src/OperadoresVerdadeiroFalso.java
@@ -0,0 +1,10 @@
+public class OperadoresVerdadeiroFalso {
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+
+        String resultado = a == b ?"Verdadeiro": "falso";
+        System.out.println(resultado);
+    }
+}
