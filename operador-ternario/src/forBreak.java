feda8624debb194a0a54376e76ec9e6e86765085
Gabriel Azambuja Silva <azambujagabriel95@gmail.com>
2024-09-20 17:58:34 -0300

    [fix] Coloquei o código testando break e continue

--- operador-ternario/src/forBreak.java
+++ operador-ternario/src/forBreak.java
@@ -1,5 +1,14 @@
 public class forBreak {
     public static void main(String[] args) {
-        
+        for (int i = 0; i<=5; i++){
+            // if (i ==3) {
+            //     System.out.println("Parei");
+            //     break;
+            // }
+            if (i == 3) {
+                continue;
+            }
+            System.out.println(i);
+        }
     }
 }
